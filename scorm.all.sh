05823a681a383fa79ae6bb062586013b72e81e4a
Adrien DUPUIS <adrien.dupuis@ez.no>
2021-09-20 08:34:00 +0200

    scorm.all.sh: Avoid jocker for next features

--- scorm.all.sh
+++ scorm.all.sh
@@ -1,2 +1,2 @@
 # ./scorm.sh OUTPUT_FILE TITLE INDEX_FILE RESOURCE_FILES…
-./scorm.sh revealjs.scorm.zip 'SCORM reveal.js Demo SCO' scorm.reveal.js.html reveal.js.md scorm.reveal.js utils.*.js;
+./scorm.sh revealjs.scorm.zip 'SCORM reveal.js Demo SCO' scorm.reveal.js.html reveal.js.md scorm.reveal.js utils.reveal.js utils.scorm.js;
