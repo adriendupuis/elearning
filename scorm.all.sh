26486ef2e55e2d282f72232ddf8b163b1c279644
Adrien Dupuis <adrien.dupuis@ibexa.co>
2021-11-29 16:04:58 +0100

    Matching question type: Update scorm.all.sh

--- scorm.all.sh
+++ scorm.all.sh
@@ -1,4 +1,4 @@
 # ./scorm.sh OUTPUT_FILE TITLE INDEX_FILE RESOURCE_FILES…
 ./scorm.sh revealjs.scorm.zip 'SCORM reveal.js Demo SCO' scorm.reveal.js.html reveal.js.md download/ scorm.reveal.js utils.reveal.js utils.scorm.js;
 openssl base64 -in gift.txt -out gift.encoded.txt;
-./scorm.sh gift.zip 'SCORM GIFT Demo SCO' scorm.gift.html gift.encoded.txt gift.md scorm.gift.js array.shuffle.js utils.scorm.js;
+./scorm.sh gift.scorm.zip 'SCORM GIFT Demo SCO' scorm.gift.html gift.encoded.txt gift.md scorm.gift.js array.shuffle.js jqueryui.matching.* utils.scorm.js;
