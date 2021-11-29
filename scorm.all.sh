8af64806e02d9cdcedc14630fc7c06b3dab21cd1
Adrien Dupuis <adrien.dupuis@ibexa.co>
2021-11-29 16:09:59 +0100

    scorm.all.sh: remove gift.encoded.txt

--- scorm.all.sh
+++ scorm.all.sh
@@ -2,3 +2,4 @@
 ./scorm.sh revealjs.scorm.zip 'SCORM reveal.js Demo SCO' scorm.reveal.js.html reveal.js.md download/ scorm.reveal.js utils.reveal.js utils.scorm.js;
 openssl base64 -in gift.txt -out gift.encoded.txt;
 ./scorm.sh gift.scorm.zip 'SCORM GIFT Demo SCO' scorm.gift.html gift.encoded.txt gift.md scorm.gift.js array.shuffle.js jqueryui.matching.* utils.scorm.js;
+rm gift.encoded.txt;
