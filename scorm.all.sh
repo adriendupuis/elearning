44516005e416b0952acc7d7e269ea220fb3594b9
Adrien Dupuis <adrien.dupuis@ibexa.co>
2021-12-15 13:04:07 +0100

    Add gift.css

--- scorm.all.sh
+++ scorm.all.sh
@@ -1,5 +1,5 @@
 # ./scorm.sh OUTPUT_FILE TITLE INDEX_FILE RESOURCE_FILES…
 ./scorm.sh revealjs.scorm.zip 'SCORM reveal.js Demo SCO' scorm.reveal.js.html reveal.js.md download/ scorm.reveal.js utils.reveal.js utils.scorm.js;
 openssl base64 -in gift.txt -out gift.encoded.txt;
-./scorm.sh gift.scorm.zip 'SCORM GIFT Demo SCO' scorm.gift.html gift.encoded.txt gift.md gift.js gift.question.js scorm.gift.js array.shuffle.js jqueryui.matching.* utils.scorm.js;
+./scorm.sh gift.scorm.zip 'SCORM GIFT Demo SCO' scorm.gift.html gift.encoded.txt gift.md gift.css gift.js gift.question.js scorm.gift.js array.shuffle.js jqueryui.matching.* utils.scorm.js;
 rm gift.encoded.txt;
