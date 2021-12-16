490735b80b475757ef8428a1835556f01c88e8ae
Adrien Dupuis <adrien.dupuis@ibexa.co>
2021-12-16 15:53:44 +0100

    .all.sh: Common seqs in vars to ease usage

--- scorm.all.sh
+++ scorm.all.sh
@@ -1,5 +1,10 @@
-# ./scorm.sh OUTPUT_FILE TITLE INDEX_FILE RESOURCE_FILES…
-./scorm.sh revealjs.scorm.zip 'SCORM reveal.js Demo SCO' scorm.reveal.js.html reveal.js.md download/ scorm.reveal.js reveal.utils.js scorm.utils.js;
+scormReveal='scorm.reveal.js reveal.utils.js scorm.utils.js';
+scormGift='gift.css gift.js scorm.gift.js array.shuffle.js jqueryui.matching.* scorm.utils.js';
+
+# ./scorm.sh OUTPUT_FILE TITLE INDEX_FILE RESOURCE_FILES… $scorm(Reveal|Gift)
+
+./scorm.sh revealjs.scorm.zip 'SCORM reveal.js Demo SCO' scorm.reveal.js.html reveal.js.md download/ $scormReveal;
+
 openssl base64 -in gift.dirty.txt -out gift.encoded.txt;
-./scorm.sh gift.scorm.zip 'SCORM GIFT Demo SCO' scorm.gift.html gift.encoded.txt gift.md gift.css gift.js scorm.gift.js array.shuffle.js jqueryui.matching.* scorm.utils.js;
+./scorm.sh gift.scorm.zip 'SCORM GIFT Demo SCO' scorm.gift.html gift.encoded.txt gift.md $scormGift;
 rm gift.encoded.txt;
