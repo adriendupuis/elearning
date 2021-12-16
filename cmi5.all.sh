d37fc1bb1a24d2cf1ede482a984be3164186dc71
Adrien Dupuis <adrien.dupuis@ibexa.co>
2021-12-16 12:36:39 +0100

    Rename GIFT example; README.md: Update GIFT & cmi5

--- cmi5.all.sh
+++ cmi5.all.sh
@@ -1,5 +1,5 @@
 # ./cmi5.sh OUTPUT_FILE TITLE INDEX_FILE RESOURCE_FILES…
 ./cmi5.sh revealjs.cmi5.zip 'cmi5 reveal.js Demo AU' cmi5.reveal.js.html reveal.js.md download/ cmi5.reveal.js reveal.utils.js cmi5.utils.js;
-openssl base64 -in gift.txt -out gift.encoded.txt;
+openssl base64 -in gift.dirty.txt -out gift.encoded.txt;
 ./cmi5.sh gift.cmi5.zip 'cmi5 GIFT Demo AU' cmi5.gift.html gift.encoded.txt gift.md gift.css gift.js cmi5.gift.js array.shuffle.js jqueryui.matching.* cmi5.utils.js;
 rm gift.encoded.txt;
