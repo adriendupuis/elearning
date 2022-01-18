cfdf79b0aa4ff520a11d3c530a400cd94d58e831
Adrien Dupuis <adrien.dupuis@ibexa.co>
2022-01-18 15:08:47 +0100

    …ui.matching.js: Snap seems better with default prepareOffsets

--- jqueryui.matching.js
+++ jqueryui.matching.js
@@ -112,50 +112,6 @@ $.widget('custom.matching', {
 // Override and fix some jQuery UI drag and drop manager methods for zoomed containers.
 // See original methods at https://github.com/jquery/jquery-ui/blob/1.13.0/ui/widgets/droppable.js#L314
 $(function () {
-    $.ui.ddmanager.prepareOffsets = function (t, event) {
-        let transform = $('.reveal > .slides').css('transform').match(/matrix\(([-.0-9]+), ([-.0-9]+), ([-.0-9]+), ([-.0-9]+), ([-.0-9]+), ([-.0-9]+)\)/);
-        let scale = transform ? transform[1] : 1;
-        let zoom = $(this).closest('section').css('zoom') ? $(this).closest('section').css('zoom') : 1;
-
-        var i, j,
-            m = $.ui.ddmanager.droppables[t.options.scope] || [],
-            type = event ? event.type : null, // workaround for #2317
-            list = (t.currentItem || t.element).find(":data(ui-droppable)").addBack();
-
-        droppablesLoop: for (i = 0; i < m.length; i++) {
-
-            // No disabled and non-accepted
-            if (m[i].options.disabled || (t && !m[i].accept.call(m[i].element[0], (t.currentItem || t.element)))) {
-                continue;
-            }
-
-            // Filter out elements in the current dragged item
-            for (j = 0; j < list.length; j++) {
-                if (list[j] === m[i].element[0]) {
-                    m[i].proportions().height = 0;
-                    continue droppablesLoop;
-                }
-            }
-
-            m[i].visible = m[i].element.css("display") !== "none";
-            if (!m[i].visible) {
-                continue;
-            }
-
-            // Activate the droppable if used directly from draggables
-            if (type === "mousedown") {
-                m[i]._activate.call(m[i], event);
-            }
-
-            m[i].offset = m[i].element.offset();
-            m[i].proportions({
-                width: m[i].element[0].offsetWidth * scale * zoom,
-                height: m[i].element[0].offsetHeight * scale * zoom
-            });
-        }
-
-    };
-
     var intersect = $.ui.intersect = (function () {
         function isOverAxis(x, reference, size) {
             return (x >= reference) && (x < (reference + size));
