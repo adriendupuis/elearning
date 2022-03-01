1623b3bcae1cd9a3bd8a9bd8e80098a5704c8bac
Adrien Dupuis <adrien.dupuis@ibexa.co>
2022-03-01 18:47:39 +0100

    Add a simple "HTML SCORM Connector"

---
+++ html.scorm.js
@@ -0,0 +1,28 @@
+$(function () {
+    let threshold = 0;
+    $(document).scroll(function () {
+        let scrollTop = $(this).scrollTop();
+        let anchor = null;
+        $('a[name]').each(function () {
+            console.log(scrollTop, $(this).offset(), $(this).position());
+            if ($(this).offset().top - scrollTop <= threshold) {
+                anchor = $(this).attr('name');
+            }
+        });
+        if (anchor) {
+            history.pushState(null, null, '#' + anchor);
+            ScormUtils.multipleSetAndSave({
+                'cmi.core.lesson_status': 'incomplete',
+                'cmi.core.exit': 'suspend',
+                'cmi.suspend_data': anchor
+            });
+        }
+        console.log(anchor);
+        if ($(document).height() - $(window).height() <= scrollTop) {
+            console.log('complete');
+            history.pushState(null, null, '#');
+            ScormUtils.complete();
+            $(document).off('scroll');
+        }
+    });
+});
